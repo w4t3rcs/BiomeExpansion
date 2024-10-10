a0f969f7d341f6989980bfaa752ab7a08898a715
w4t3rcs <w4t3rofficial@gmail.com>
2024-10-10 23:53:09 +0200

    Added Ore generation

---
+++ Helpers/OreHelper.cs
@@ -0,0 +1,20 @@
+﻿using BiomeExpansion.Common.Dtos;
+using Terraria;
+
+namespace BiomeExpansion.Helpers;
+
+public static class OreHelper
+{
+    public static void GenerateOre(BEBiome biome, int startY, int endY, sbyte rarity, float strength, int steps, ushort ore)
+    {
+        var (leftX, rightX) = BiomeHelper.BEBiomesXCoordinates[biome];
+        for (int x = leftX; x < rightX; x++)
+        {
+            for (int y = startY; y < endY; y++)
+            {
+                if (WorldGen.genRand.NextBool(rarity)) 
+                    WorldGen.OreRunner(x, y, strength, steps, ore);
+            }
+        }
+    }
+}
