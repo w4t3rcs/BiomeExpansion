398e377bf899ac74ba1a0a179a0ff9ef69909cd3
w4t3rcs <w4t3rofficial@gmail.com>
2024-10-11 16:19:55 +0200

    Added bows

---
+++ Content/Items/Weapons/CorruptoomBow.cs
@@ -0,0 +1,30 @@
+﻿using BiomeExpansion.Helpers;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace BiomeExpansion.Content.Items.Weapons;
+
+public class CorruptoomBow : ModItem
+{
+    public override string Texture => TextureHelper.GetDynamicItemTexture("CorruptoomBow");
+
+    public override void SetDefaults()
+    {
+        Item.width = 18;
+        Item.height = 40;
+        Item.useStyle = ItemUseStyleID.Shoot;
+        Item.useTime = 12;
+        Item.useAnimation = 18;
+        Item.reuseDelay = 18;
+        Item.useLimitPerAnimation = 1;
+        Item.DamageType = DamageClass.Ranged;
+        Item.damage = 16;
+        Item.knockBack = 1;
+        Item.crit = 6;
+        Item.UseSound = SoundID.Item5;
+        Item.noMelee = true;
+        Item.shoot = ProjectileID.WoodenArrowFriendly;
+        Item.shootSpeed = 7f;
+        Item.useAmmo = AmmoID.Arrow;
+    }
+}
