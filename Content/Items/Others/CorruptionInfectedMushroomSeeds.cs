ae2227669b82463d3a08586dd428908d8aee39b4
w4t3rcs <w4t3rofficial@gmail.com>
2024-10-11 16:20:15 +0200

    Added seeds

---
+++ Content/Items/Others/CorruptionInfectedMushroomSeeds.cs
@@ -0,0 +1,46 @@
+﻿using BiomeExpansion.Content.Tiles;
+using BiomeExpansion.Helpers;
+using Terraria;
+using Terraria.Audio;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace BiomeExpansion.Content.Items.Others;
+
+public class CorruptionInfectedMushroomSeeds : ModItem
+{
+    public override string Texture => TextureHelper.GetDynamicItemTexture("CorruptionInfectedMushroomSeeds");
+    
+    public override void SetStaticDefaults()
+    {
+        ItemID.Sets.DisableAutomaticPlaceableDrop[Type] = true;
+        Item.ResearchUnlockCount = 25;
+    }
+
+    public override void SetDefaults()
+    {
+        Item.width = 12;
+        Item.height = 12;
+        Item.useTurn = true;
+        Item.autoReuse = true;
+        Item.consumable = true;
+        Item.useTime = 10;
+        Item.useAnimation = 15;
+        Item.useStyle = ItemUseStyleID.Swing;
+        Item.maxStack = 9999;
+    }
+    
+    public override bool? UseItem(Player player)
+    {
+        Tile tile = Framing.GetTileSafely(Player.tileTargetX, Player.tileTargetY);
+        if (tile.HasTile && tile.TileType == ModContent.TileType<InfectedMushroomDirt>() && player.IsInTileInteractionRange(Player.tileTargetX, Player.tileTargetY, TileReachCheckSettings.Simple))
+        {
+            Main.tile[Player.tileTargetX, Player.tileTargetY].TileType = (ushort)ModContent.TileType<CorruptionInfectedMushroomGrass>();
+            SoundEngine.PlaySound(SoundID.Dig, player.Center);
+            return true;
+        }
+
+        return false;
+    }
+}
