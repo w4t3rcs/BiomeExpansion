cb68e4884c9e43b5486df407672cdcdc1c0e789e
w4t3rcs <w4t3rofficial@gmail.com>
2024-10-23 00:23:02 +0200

    Slightly fixed fear piece (at least I've tried  :P)

--- Content/Items/Others/FearPiece.cs
+++ Content/Items/Others/FearPiece.cs
@@ -1,7 +1,9 @@
 ﻿using BiomeExpansion.Helpers;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using Terraria;
 using Terraria.DataStructures;
+using Terraria.GameContent;
 using Terraria.ID;
 using Terraria.ModLoader;
 
@@ -13,7 +15,7 @@ public class FearPiece : ModItem
     
     public override void SetStaticDefaults()
     {
-        Main.RegisterItemAnimation(Item.type, new DrawAnimationVertical(10, 7));
+        Main.RegisterItemAnimation(Item.type, new DrawAnimationVertical(8, 7));
         ItemID.Sets.AnimatesAsSoul[Item.type] = true;
         ItemID.Sets.ItemNoGravity[Item.type] = true;  
         Item.ResearchUnlockCount = 25;
@@ -25,9 +27,18 @@ public class FearPiece : ModItem
         Item.height = 60;
         Item.maxStack = Item.CommonMaxStack;
         Item.value = 1000;
-        Item.rare = ItemRarityID.Orange;
+        Item.rare = ItemRarityID.LightRed;
     }
-    
+
+    public override bool PreDrawInInventory(SpriteBatch spriteBatch, Vector2 position, Rectangle frame, Color drawColor, Color itemColor,
+        Vector2 origin, float scale)
+    {
+        const float newScale = 1f;
+        position += new Vector2(0f, -4) * newScale;
+        spriteBatch.Draw(TextureAssets.Item[Type].Value, position, frame, drawColor, 0f, origin, newScale, SpriteEffects.None, 0);
+        return false;
+    }
+
     public override void PostUpdate() {
         Lighting.AddLight(Item.Center, Color.Red.ToVector3() * 0.75f * Main.essScale);
     }
