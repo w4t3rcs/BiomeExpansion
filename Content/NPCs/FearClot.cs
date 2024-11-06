6f2ea1c6cd8a3f586e0ba11aa271928afa6d11f3
w4t3rcs <w4t3rofficial@gmail.com>
2024-11-06 18:16:18 +0100

    Added Fear Clot

---
+++ Content/NPCs/FearClot.cs
@@ -0,0 +1,75 @@
+using BiomeExpansion.Content.Biomes;
+using BiomeExpansion.Content.Buffs;
+using BiomeExpansion.Helpers;
+using Terraria;
+using Terraria.GameContent.Bestiary;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace BiomeExpansion.Content.NPCs;
+
+public class FearClot : ModNPC
+{
+    public override string Texture => TextureHelper.DynamicNPCsTextures["FearClot"];
+
+    public override void SetStaticDefaults()
+    {
+        Main.npcFrameCount[NPC.type] = 4;
+    }
+
+    public override void SetDefaults()
+    {
+        NPC.aiStyle = NPCAIStyleID.HoveringFighter;
+        AIType = NPCID.Ghost;
+        NPC.damage = 21;
+        NPC.width = 54;
+        NPC.height = 32;
+        NPC.defense = 6;
+        NPC.lifeMax = 24;
+        NPC.knockBackResist = 0.5f;
+        NPC.noGravity = true;
+        AnimationType = NPCID.Pixie;
+        NPC.value = Item.buyPrice(0, 0, 5, 0);
+        NPC.HitSound = SoundID.NPCHit1;
+        NPC.DeathSound = SoundID.NPCDeath1;
+        Banner = NPC.type;
+        SpawnModBiomes = [ModContent.GetInstance<CorruptionInfectedMushroomSurfaceBiome>().Type, ModContent.GetInstance<CrimsonInfectedMushroomSurfaceBiome>().Type];
+        // BannerItem = ModContent.ItemType<>();
+        NPCHelper.AdjustExpertMode(NPC);
+        NPCHelper.AdjustMasterMode(NPC);
+    }
+
+    public override void SetBestiary(BestiaryDatabase database, BestiaryEntry bestiaryEntry)
+    {
+        bestiaryEntry.Info.AddRange(new IBestiaryInfoElement[]
+        {
+            BestiaryDatabaseNPCsPopulator.CommonTags.SpawnConditions.Biomes.TheCorruption,
+            new FlavorTextBestiaryInfoElement("Mods.BiomeExpansion.Bestiary.FearClot")
+        });
+    }
+
+    public override float SpawnChance(NPCSpawnInfo spawnInfo)
+    {
+        if (spawnInfo.Player.InModBiome<CorruptionInfectedMushroomSurfaceBiome>() || spawnInfo.Player.InModBiome<CrimsonInfectedMushroomSurfaceBiome>())
+        {
+            return 0.25f;
+        }
+
+        return 0f;
+    }
+
+    public override void OnHitPlayer(Player target, Player.HurtInfo hurtInfo)
+    {
+        if (hurtInfo.Damage > 0)
+        {
+            if (WorldGen.crimson) target.AddBuff(ModContent.BuffType<CrimsonSporeInfectionDebuff>(), 2400, true);
+            else target.AddBuff(ModContent.BuffType<CorruptionSporeInfectionDebuff>(), 2400, true);
+        }
+    }
+
+    public override void HitEffect(NPC.HitInfo hit)
+    {
+        if (WorldGen.crimson) NPCHelper.DoHitDust(NPC, hit.HitDirection, DustID.Crimson);
+        else NPCHelper.DoHitDust(NPC, hit.HitDirection, DustID.Corruption);
+    }
+}
