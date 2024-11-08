f88aedd2975ab86dcd12b63eebb0332a1dc6d9ee
HaHAjax <128398800+HaHAjax@users.noreply.github.com>
2024-11-08 13:47:39 -0700

    Add CorruptionInfectedDragonfly

---
+++ Content/NPCs/CorruptionInfectedDragonfly.cs
@@ -0,0 +1,61 @@
+﻿using System.IO;
+using System.Numerics;
+using BiomeExpansion.Content.Biomes;
+using BiomeExpansion.Content.Buffs;
+using BiomeExpansion.Helpers;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Design;
+using Terraria;
+using Terraria.GameContent.Bestiary;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace BiomeExpansion.Content.NPCs
+{
+    public class CorruptionInfectedDragonfly : ModNPC
+    {
+        //private const int ClonedNPCID = NPCID.BlackDragonfly;
+
+        public override string Texture => TextureHelper.DynamicNPCsTextures["CorruptionInfectedDragonflyNPC"];
+
+        public override void SetStaticDefaults()
+        {
+            Main.npcFrameCount[NPC.type] = 4;
+            Main.npcCatchable[NPC.type] = true;
+
+            NPCID.Sets.CountsAsCritter[NPC.type] = true;
+            NPCID.Sets.TakesDamageFromHostilesWithoutBeingFriendly[NPC.type] = true;
+
+            // Change and uncomment the next line if the fly should be immune to anything
+            //NPCID.Sets.SpecificDebuffImmunity[Type][BuffID.INSERT_DEBUFF_HERE] = true;
+        }
+
+        public override void SetBestiary(BestiaryDatabase database, BestiaryEntry bestiaryEntry)
+        {
+            bestiaryEntry.Info.AddRange(new IBestiaryInfoElement[]
+            {
+            BestiaryDatabaseNPCsPopulator.CommonTags.SpawnConditions.Biomes.TheCorruption,
+            new FlavorTextBestiaryInfoElement("Mods.BiomeExpansion.Bestiary.CorruptionInfectedFly")
+            });
+        }
+
+        public override void SetDefaults()
+        {
+            // Set NPC.catchItem here
+            SpawnModBiomes = [ModContent.GetInstance<CorruptionInfectedMushroomSurfaceBiome>().Type];
+            NPC.CloneDefaults(NPCID.BlackDragonfly);
+
+            AIType = NPCID.BlackDragonfly;
+            AnimationType = NPCID.BlackDragonfly;
+
+            // When an item sprite is made, uncomment the next line
+            //NPC.catchItem = ModContent.ItemType<CorruptionInfectedFlyNPC>();
+        }
+    }
+
+    // Uncomment this code and put some stuff in when an item sprite is made
+    //public class CorruptionInfectedFlyItem : ModItem
+    //{
+
+    //}
+}
