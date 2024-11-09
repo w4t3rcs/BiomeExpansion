c1e6607f241ac9125b0f71996185bc43d281b71c
HaHAjax <128398800+HaHAjax@users.noreply.github.com>
2024-11-08 18:51:58 -0700

    Add CILadybug, CIStagBeetle

---
+++ Content/NPCs/CorruptionInfectedStagBeetle.cs
@@ -0,0 +1,53 @@
+﻿using System.IO;
+using BiomeExpansion.Content.Biomes;
+using BiomeExpansion.Content.Buffs;
+using BiomeExpansion.Helpers;
+using Terraria;
+using Terraria.GameContent.Bestiary;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace BiomeExpansion.Content.NPCs
+{
+    public class CorruptionInfectedStagBeetle : ModNPC
+    {
+        public override string Texture => TextureHelper.DynamicNPCsTextures["CorruptionInfectedStagBeetle"];
+
+        public override void SetStaticDefaults()
+        {
+            Main.npcFrameCount[NPC.type] = 8;
+            Main.npcCatchable[NPC.type] = true;
+            NPCID.Sets.CountsAsCritter[NPC.type] = true;
+            NPCID.Sets.TakesDamageFromHostilesWithoutBeingFriendly[NPC.type] = true;
+
+        }
+
+        public override void SetDefaults()
+        {
+            NPC.CloneDefaults(NPCID.LadyBug);
+            AnimationType = NPCID.LadyBug;
+            AIType = NPCID.LadyBug;
+            NPC.aiStyle = NPCAIStyleID.Ladybug;
+            SpawnModBiomes = [ModContent.GetInstance<CorruptionInfectedMushroomSurfaceBiome>().Type];
+        }
+
+        public override void SetBestiary(BestiaryDatabase database, BestiaryEntry bestiaryEntry)
+        {
+            bestiaryEntry.Info.AddRange(new IBestiaryInfoElement[]
+            {
+            BestiaryDatabaseNPCsPopulator.CommonTags.SpawnConditions.Biomes.TheCorruption,
+            new FlavorTextBestiaryInfoElement("Mods.BiomeExpansion.Bestiary.CorruptionInfectedStagBeetle")
+            });
+        }
+
+        public override float SpawnChance(NPCSpawnInfo spawnInfo)
+        {
+            if (spawnInfo.Player.InModBiome<CorruptionInfectedMushroomSurfaceBiome>() && spawnInfo.Player.ZoneDirtLayerHeight) // Edit this as you want
+            {
+                return 0.1f;
+            }
+
+            return 0f;
+        }
+    }
+}
