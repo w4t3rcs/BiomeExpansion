bdb5f9822b3465425dfda58a6fa069454d1581c1
HaHAjax <128398800+HaHAjax@users.noreply.github.com>
2024-11-08 17:43:27 -0700

    Add CIButterfly and CICaterpillar
    Added CorruptionInfectedButterfly and CorruptionInfectedCaterpillar
    (CI stands for CorruptionInfected)

---
+++ Content/NPCs/CorruptionInfectedButterfly.cs
@@ -0,0 +1,53 @@
+﻿using System.IO;
+using BiomeExpansion.Content.Biomes;
+using BiomeExpansion.Content.Buffs;
+using BiomeExpansion.Helpers;
+using Terraria;
+using Terraria.GameContent.Bestiary;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace BiomeExpansion.Content.NPCs
+{
+    public class CorruptionInfectedButterfly : ModNPC
+    {
+        public override string Texture => TextureHelper.DynamicNPCsTextures["CorruptionInfectedButterfly"];
+
+        public override void SetStaticDefaults()
+        {
+            Main.npcFrameCount[NPC.type] = 3;
+            Main.npcCatchable[NPC.type] = true;
+            NPCID.Sets.CountsAsCritter[NPC.type] = true;
+            NPCID.Sets.TakesDamageFromHostilesWithoutBeingFriendly[NPC.type] = true;
+
+        }
+
+        public override void SetDefaults()
+        {
+            NPC.CloneDefaults(NPCID.Butterfly);
+            AnimationType = NPCID.Butterfly;
+            AIType = NPCID.Butterfly;
+            NPC.aiStyle = NPCAIStyleID.Butterfly;
+            SpawnModBiomes = [ModContent.GetInstance<CorruptionInfectedMushroomSurfaceBiome>().Type];
+        }
+
+        public override void SetBestiary(BestiaryDatabase database, BestiaryEntry bestiaryEntry)
+        {
+            bestiaryEntry.Info.AddRange(new IBestiaryInfoElement[]
+            {
+            BestiaryDatabaseNPCsPopulator.CommonTags.SpawnConditions.Biomes.TheCorruption,
+            new FlavorTextBestiaryInfoElement("Mods.BiomeExpansion.Bestiary.CorruptionInfectedButterfly")
+            });
+        }
+
+        public override float SpawnChance(NPCSpawnInfo spawnInfo)
+        {
+            if (spawnInfo.Player.InModBiome<CorruptionInfectedMushroomSurfaceBiome>() && spawnInfo.Player.ZoneOverworldHeight)
+            {
+                return 0.1f;
+            }
+
+            return 0f;
+        }
+    }
+}
---
+++ Content/NPCs/CorruptionInfectedCaterpillar.cs
@@ -0,0 +1,59 @@
+﻿using System.IO;
+using BiomeExpansion.Content.Biomes;
+using BiomeExpansion.Content.Buffs;
+using BiomeExpansion.Helpers;
+using Terraria;
+using Terraria.GameContent.Bestiary;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ModLoader.Utilities;
+
+namespace BiomeExpansion.Content.NPCs
+{
+    public class CorruptionInfectedCaterpillar : ModNPC
+    {
+        public override string Texture => TextureHelper.DynamicNPCsTextures["CorruptionInfectedCaterpillar"];
+
+        public override void SetStaticDefaults()
+        {
+            Main.npcFrameCount[NPC.type] = 4;
+            Main.npcCatchable[NPC.type] = true;
+            NPCID.Sets.CountsAsCritter[NPC.type] = true;
+            NPCID.Sets.TakesDamageFromHostilesWithoutBeingFriendly[NPC.type] = true;
+
+        }
+
+        public override void SetDefaults()
+        {
+            NPC.CloneDefaults(NPCID.Grubby);
+            AnimationType = NPCID.Grubby;
+            AIType = NPCID.Grubby;
+            NPC.aiStyle = 66;
+            SpawnModBiomes = [ModContent.GetInstance<CorruptionInfectedMushroomSurfaceBiome>().Type];
+        }
+
+        public override void SetBestiary(BestiaryDatabase database, BestiaryEntry bestiaryEntry)
+        {
+            bestiaryEntry.Info.AddRange(new IBestiaryInfoElement[]
+            {
+            BestiaryDatabaseNPCsPopulator.CommonTags.SpawnConditions.Biomes.TheCorruption,
+            new FlavorTextBestiaryInfoElement("Mods.BiomeExpansion.Bestiary.CorruptionInfectedButterfly")
+            });
+        }
+
+        public override float SpawnChance(NPCSpawnInfo spawnInfo)
+        {
+            //if (spawnInfo.Player.InModBiome<CorruptionInfectedMushroomSurfaceBiome>() && spawnInfo.Player.ZoneOverworldHeight)
+            //{
+            //    return 0.1f;
+            //}
+
+            //return 0f;
+
+
+            // this isn't final, we still need to change the biome its in
+            return SpawnCondition.WormCritter.Chance * 0.1f;
+
+        }
+    }
+}
