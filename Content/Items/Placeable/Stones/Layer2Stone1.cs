79d48728ad5e9f8efb2110c459ba1c838c203edc
HaHAjax <128398800+HaHAjax@users.noreply.github.com>
2024-11-16 14:32:18 -0700

    Add Stone 1 & 2 for Layer 2

---
+++ Content/Items/Placeable/Stones/Layer2Stone1.cs
@@ -0,0 +1,19 @@
+﻿namespace BiomeExpansion.Content.Items.Placeable.Stones
+{
+    public class Layer2Stone1 : ModItem
+    {
+        public override string Texture => TextureHelper.DynamicItemsTextures["Layer2Stone1"];
+
+        public override void SetStaticDefaults()
+        {
+            Item.ResearchUnlockCount = 100;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.DefaultToPlaceableTile(ModContent.TileType<Tiles.Stones.Layer2Stone1>());
+            Item.width = 16;
+            Item.height = 16;
+        }
+    }
+}
