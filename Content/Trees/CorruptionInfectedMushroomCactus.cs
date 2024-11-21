8b57aa0ea0bad10486b29c34265bb08c76671c6e
w4t3rcs <w4t3rofficial@gmail.com>
2024-11-21 01:15:15 +0100

    Cactus doesn't work :p

---
+++ Content/Trees/CorruptionInfectedMushroomCactus.cs
@@ -0,0 +1,19 @@
+﻿using BiomeExpansion.Content.Items.Placeable.Furniture;
+using BiomeExpansion.Content.Tiles.Biome;
+using BiomeExpansion.Content.Tiles.Plants;
+using BiomeExpansion.Content.Tiles.Sands;
+using ReLogic.Content;
+using Terraria.GameContent;
+
+namespace BiomeExpansion.Content.Trees;
+
+public class CorruptionInfectedMushroomCactus : ModCactus
+{
+    public override Asset<Texture2D> GetTexture() => ModContent.Request<Texture2D>("BiomeExpansion/Assets/Trees/CorruptionInfectedMushroomCactus");
+    public override Asset<Texture2D> GetFruitTexture() => null;
+    
+    public override void SetStaticDefaults() 
+    {
+        GrowsOnTileId = [ModContent.TileType<CorruptionInfectedMushroomSand>()];
+    }
+}
---
+++ Content/Trees/CrimsonInfectedMushroomCactus.cs
@@ -0,0 +1,19 @@
+﻿using BiomeExpansion.Content.Items.Placeable.Furniture;
+using BiomeExpansion.Content.Tiles.Biome;
+using BiomeExpansion.Content.Tiles.Plants;
+using BiomeExpansion.Content.Tiles.Sands;
+using ReLogic.Content;
+using Terraria.GameContent;
+
+namespace BiomeExpansion.Content.Trees;
+
+public class CrimsonInfectedMushroomCactus : ModCactus
+{
+    public override Asset<Texture2D> GetTexture() => ModContent.Request<Texture2D>("BiomeExpansion/Assets/Trees/CrimsonInfectedMushroomCactus");
+    public override Asset<Texture2D> GetFruitTexture() => null;
+    
+    public override void SetStaticDefaults() 
+    {
+        GrowsOnTileId = [ModContent.TileType<CrimsonInfectedMushroomSand>()];
+    }
+}
